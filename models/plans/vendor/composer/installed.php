5652d11f0ded23c35fb52f0717452b0a7aca4319
AGL1709 <alangabryel17092002@gmail.com>
2025-09-23 19:19:06 -0300

    Adiciona workflow PHP + Composer

--- models/plans/vendor/composer/installed.php
+++ models/plans/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '27bb9ad55670b10cbfea93cb74a3d5d5034925e2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '27bb9ad55670b10cbfea93cb74a3d5d5034925e2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'mercadopago/dx-php' => array(
-            'pretty_version' => '3.2.0',
-            'version' => '3.2.0.0',
-            'reference' => '84a30e5f23c5d722298c6faeffe574a2777c1439',
+            'pretty_version' => '3.7.0',
+            'version' => '3.7.0.0',
+            'reference' => '1805d569d427c6f96baf71a18144193a64872904',
             'type' => 'library',
             'install_path' => __DIR__ . '/../mercadopago/dx-php',
             'aliases' => array(),
